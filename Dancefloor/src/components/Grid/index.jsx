ff4f4dec6b6b69f7c7fdf503926ec75ffd07995e
ChingT <j60626j@gmail.com>
2023-09-07 14:53:41 +0200

    Create getRandomColor

--- Dancefloor/src/components/Grid/index.jsx
+++ Dancefloor/src/components/Grid/index.jsx
@@ -1,11 +1,20 @@
 import "./styles.css";
 import Cell from "../Cell";
 
+const getRandomColor = () => {
+  const randomInt = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+  const h = randomInt(0, 360);
+  const s = randomInt(42, 98);
+  const l = randomInt(40, 90);
+  return `hsl(${h},${s}%,${l}%)`;
+};
+
 function Grid() {
   const generateCells = (row = 5, col = 5) => {
-    let color = "orange";
     return Array.from({ length: row * col }).map((_, index) => (
-      <Cell key={index} color={color} index={index} />
+      <Cell key={index} color={getRandomColor()} />
     ));
   };
 
